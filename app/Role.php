72bc959a1af784bc7dd04f04a1cd27e0dd63c866
Rocher55 <pierre.castanie@etu.iut-tlse3.fr>
2019-05-31 12:39:09 +0200

    Ajout de Authenticate et CheckForMaintenanceMode middleware

---
+++ app/Role.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends \TCG\Voyager\Models\Role
+{
+    /*
+    public function users(){
+        $this->hasMany('App\User');
+    }
+
+    public function userss(){
+        $this->belongsToMany('App\User','user_roles');
+    }
+*/
+}
